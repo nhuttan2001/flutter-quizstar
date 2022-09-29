cbefa45eb655fbd87c702e9afd3ac2e1bdd62bf7
nhuttan2001 <93700491+nhuttan2001@users.noreply.github.com>
2022-09-29 19:19:34 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:quizstar/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  var testWidgets = testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -24,7 +24,14 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
+    var find;
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+}
+
+class Icons {
+}
+
+void expect(text, findsOneWidget) {
 }
